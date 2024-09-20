439468cf3e07ff4c9b16299c4b4d551f11d678a7
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2024-09-20 15:33:58 +0200

    Fix jq command error for in-place file change

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ rm -rf pkg pkg-node pkg-web
 wasm-pack build --target nodejs
 mv pkg pkg-node
 wasm-pack build --target web
-jq '.name = "uplc-wasm-web"' pkg/package.json > pkg/package.json
+jq '.name = "uplc-wasm-web"' pkg/package.json > pkgtemp.json && mv pkgtemp.json pkg/package.json
 mv pkg pkg-web
