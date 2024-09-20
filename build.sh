562b99a1fb602dd73d2460d4753b95bd88e93ef2
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2024-09-20 15:22:42 +0200

    Auto rename the web target to uplc-wasm-web

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ rm -rf pkg pkg-node pkg-web
 wasm-pack build --target nodejs
 mv pkg pkg-node
 wasm-pack build --target web
+jq '.name = "uplc-wasm-web"' pkg/package.json > pkg/package.json
 mv pkg pkg-web
